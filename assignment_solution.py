d9d51ec9001cf96032b22313887e39ab6f17e56e
asifdotpy <asifdotpy@gmail.com>
2022-11-19 00:02:22 +0600

    variable assignment complete

--- assignment_solution.py
+++ assignment_solution.py
@@ -12,70 +12,70 @@ riders_list = [
         "pickup_time": 5,
         "pickup_address": "47 W 13th St, New York, NY 10011, USA",
         "pickup_location": [40.820240, -73.935944],
-        "drop_off_address": "44 W 4th St, New York, NY 10012, USA"
+        "dropoff_address": "44 W 4th St, New York, NY 10012, USA"
     },
     {
         "rider_id": 2,
         "pickup_time": 6,
         "pickup_address": "20 Cooper Square, New York, NY 10003, USA",
         "pickup_location": [40.727840, -73.991539],
-        "drop_off_address": "33 Washington Square W, New York, NY 10011, USA\r"
+        "dropoff_address": "33 Washington Square W, New York, NY 10011, USA\r"
     },
     {
         "rider_id": 3,
         "pickup_time": 8,
         "pickup_address": "1 E 2nd St, New York, NY 10003, USA",
         "pickup_location": [40.725239, -73.991508],
-        "drop_off_address": "129 Washington streey e New York NY 10011 USA"
+        "dropoff_address": "129 Washington streey e New York NY 10011 USA"
     },
     {
         "rider_id": 4,
         "pickup_time": 13,
         "pickup_address": "75 3rd Ave, New York, NY 10003, USA",
         "pickup_location": [41.929192, -73.981262],
-        "drop_off_address": "124 Broadway  New York NY 10025 USA"
+        "dropoff_address": "124 Broadway  New York NY 10025 USA"
     },
     {
         "rider_id": 5,
         "pickup_time": 9,
         "pickup_address": "Metrotech Center, Brooklyn, NY 11201, USA",
         "pickup_location": [40.7313843, -73.9882642],
-        "drop_off_address": "502 Delency Street New York NY 1025"
+        "dropoff_address": "502 Delency Street New York NY 1025"
     },
     {
         "rider_id": 6,
         "pickup_time": 21,
         "pickup_address": "40 E 7th St, New York, NY 10003, USA",
         "pickup_location": [40.728062, -73.988819],
-        "drop_off_address": "536 Clinton Street New York NY 1100"
+        "dropoff_address": "536 Clinton Street New York NY 1100"
     },
     {
         "rider_id": 7,
         "pickup_time": 5,
         "pickup_address": "838 Broadway, New York, NY 10003, USA",
         "pickup_location": [40.7336279, -73.9908577],
-        "drop_off_address": "105 Housetone Street New York NY 1200"
+        "dropoff_address": "105 Housetone Street New York NY 1200"
     },
     {
         "rider_id": 8,
         "pickup_time": 12,
         "pickup_address": "69 W 9th St, New York, NY 10011, USA",
         "pickup_location": [40.7342159, -73.9988314],
-        "drop_off_address": "201 lexinton Avinue New York NY 20000"
+        "dropoff_address": "201 lexinton Avinue New York NY 20000"
     },
     {
         "rider_id": 9,
         "pickup_time": 10,
         "pickup_address": "371 7th Ave, New York, NY 10001",
         "pickup_location": [40.748712, -73.9916585],
-        "drop_off_address": "102 Time Square New York NY 2001"
+        "dropoff_address": "102 Time Square New York NY 2001"
     },
     {
         "rider_id": 10,
         "pickup_time": 13,
         "pickup_address": "400 Broome St, New York, NY 10013, USA",
         "pickup_location": [40.72082, -73.997398],
-        "drop_off_address": "524 Ozone Park New York NY 20000."
+        "dropoff_address": "524 Ozone Park New York NY 20000."
     }
 ]
 
@@ -89,14 +89,15 @@ class Driver:
 
 # Rider class instantiated
 class Rider:
-    def __init__(self, pickup_time, pickup_location, pickup_address, dropOff_address):
+    def __init__(self, rider_id, pickup_time, pickup_address, pickup_location, dropOff_address):
         # pickup_location must be a list with only 2 elements first one is latitude and the second one is longitude
+        self.rider_id = rider_id
         self.pickup_time = pickup_time
         self.pickup_location = pickup_location
         self.pickup_address = pickup_address  # pickup_address
         self.dropOff_address = dropOff_address
-        self.dropoff_location = self.dropoff_location_calc()
-        self.estimated_ride_duration = self.ride_duration_calc()
+        #self.dropoff_location = self.dropoff_location_calc()
+        #self.estimated_ride_duration = self.ride_duration_calc()
         # ride duration calculator
 
         def ride_duration_calc(self):
@@ -112,6 +113,19 @@ class Rider:
 # Rider class instances will have a prefered ride
 
 
+# Creating dynamic variables for Rider class
+passengers = {}  # Container for dummy riders
+for count, value in enumerate(riders_list):
+    key = str("rider" + str(count + 1))
+    passengers[key] = value
+
+# Executing key as variable name and value as value e.g. rider1 = {...}
+for key, value in passengers.items():
+    exec(f"{key} = {value}")
+    exec(f"{key} = Rider(rider_id={key}['rider_id'], pickup_time={key}['pickup_time'], pickup_address={key}['pickup_address'], pickup_location={key}['pickup_location'], dropOff_address={key}['dropoff_address'])")
+
 if __name__ == "__main__":
-    driver1 = Driver("New York", is_available=False)
-    print(driver1.is_available)
+    # Creating objects of Rider from passengers list
+    # print(rider1)
+
+    print(rider5.pickup_time)
--- test.py
+++ test.py
@@ -1,21 +1,76 @@
-class Driver:
-    def __init__(self, id, is_available=True):
-        self.id = id
-        self.is_available = is_available
+riders_list = [
+    {
+        "rider_id": 1,
+        "pickup_time": 5,
+        "pickup_address": "47 W 13th St, New York, NY 10011, USA",
+        "pickup_location": [40.820240, -73.935944],
+        "dropoff_address": "44 W 4th St, New York, NY 10012, USA"
+    },
+    {
+        "rider_id": 2,
+        "pickup_time": 6,
+        "pickup_address": "20 Cooper Square, New York, NY 10003, USA",
+        "pickup_location": [40.727840, -73.991539],
+        "dropoff_address": "33 Washington Square W, New York, NY 10011, USA\r"
+    },
+    {
+        "rider_id": 3,
+        "pickup_time": 8,
+        "pickup_address": "1 E 2nd St, New York, NY 10003, USA",
+        "pickup_location": [40.725239, -73.991508],
+        "dropoff_address": "129 Washington streey e New York NY 10011 USA"
+    },
+    {
+        "rider_id": 4,
+        "pickup_time": 13,
+        "pickup_address": "75 3rd Ave, New York, NY 10003, USA",
+        "pickup_location": [41.929192, -73.981262],
+        "dropoff_address": "124 Broadway  New York NY 10025 USA"
+    },
+    {
+        "rider_id": 5,
+        "pickup_time": 9,
+        "pickup_address": "Metrotech Center, Brooklyn, NY 11201, USA",
+        "pickup_location": [40.7313843, -73.9882642],
+        "dropoff_address": "502 Delency Street New York NY 1025"
+    },
+    {
+        "rider_id": 6,
+        "pickup_time": 21,
+        "pickup_address": "40 E 7th St, New York, NY 10003, USA",
+        "pickup_location": [40.728062, -73.988819],
+        "dropoff_address": "536 Clinton Street New York NY 1100"
+    },
+    {
+        "rider_id": 7,
+        "pickup_time": 5,
+        "pickup_address": "838 Broadway, New York, NY 10003, USA",
+        "pickup_location": [40.7336279, -73.9908577],
+        "dropoff_address": "105 Housetone Street New York NY 1200"
+    },
+    {
+        "rider_id": 8,
+        "pickup_time": 12,
+        "pickup_address": "69 W 9th St, New York, NY 10011, USA",
+        "pickup_location": [40.7342159, -73.9988314],
+        "dropoff_address": "201 lexinton Avinue New York NY 20000"
+    },
+    {
+        "rider_id": 9,
+        "pickup_time": 10,
+        "pickup_address": "371 7th Ave, New York, NY 10001",
+        "pickup_location": [40.748712, -73.9916585],
+        "dropoff_address": "102 Time Square New York NY 2001"
+    },
+    {
+        "rider_id": 10,
+        "pickup_time": 13,
+        "pickup_address": "400 Broome St, New York, NY 10013, USA",
+        "pickup_location": [40.72082, -73.997398],
+        "dropoff_address": "524 Ozone Park New York NY 20000."
+    }
+]
 
 
-class Rider:
-    def __init__(self, location):
-        self.location = location
-
-    def change_driver_availablity(self):
-        driver2.is_available = False
-
-
-driver1 = Driver(1, is_available=False)
-driver2 = Driver(2)
-print(driver2.is_available)
-
-rider1 = Rider("new york")
-rider1.change_driver_availablity()
-print(f"Driver 2 availablity changed to {driver2.is_available}")
+for i in riders_list:
+    print(i["rider_id"])
